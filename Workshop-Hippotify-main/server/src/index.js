edd3b1846e7dd0afd40bd6a767ac78cd8e56c8c6
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-02-01 10:53:19 +0700

    add

---
+++ Workshop-Hippotify-main/server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import connection from "./config/database.js";
+import playlistRoute from "./routes/playlistRoute.js";
+import userRoute from "./routes/userRoute.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/api/playlists", playlistRoute);
+app.use("/api/users", userRoute);
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database is connected");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
